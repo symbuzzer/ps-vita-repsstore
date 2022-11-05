ccb4302b398a896a1cbdd013e15f8de5a2788f27
Ali Beyaz <symbuzzer@users.noreply.github.com>
2022-11-06 02:09:30 +0300

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,7 @@
+splash.zoom("sce_sys/livearea/contents/bg.png")
+if os.access() == 0 then
+if back2 then back2:blit(0,0) end
+screen.flip()
+os.message("\n\n\nEnable unsafe homebrew")os.exit()
+end
+os.uri("psns: ")
